92236c297799f26829bb45cea5804561b2e58b77
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-05 07:40:30 +0300

    Create initial migration file 0001__initial.py
    Initial migration file created for database schema.

---
+++ migrations/versions/0001__initial.py
@@ -0,0 +1,23 @@
+"""empty message
+
+Revision ID: 0001
+Revises:
+Create Date: 2026-09-05
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+revision: str = "0001"
+down_revision: str | None = None
+branch_labels: str | None = None
+depends_on: str | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
