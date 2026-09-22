f1a1c7ea784a346025084a024a32b7c1d8bb4397
6838gamal <89938004+6838gamal@users.noreply.github.com>
2026-09-22 19:18:37 +0300

    Update database.py

--- app/config/database.py
+++ app/config/database.py
@@ -1,15 +1,18 @@
+# app/config/database.py
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.config.settings import settings
 
+# ✅ استبدل asyncpg بـ psycopg2
+db_url = settings.database_url.replace("+asyncpg", "+psycopg2")
+
 engine = create_engine(
-    settings.database_url,
+    db_url,
     pool_pre_ping=True,
     echo=settings.is_dev,
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
 
 
